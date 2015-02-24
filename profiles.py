44855b8478f9646099232b1f27c81b49cfb597eb
A <a@example.org>
2015-02-23 21:16:23 -0500

    reduced division count

--- profiles.py
+++ profiles.py
@@ -36,18 +36,19 @@ class Profile:
             self.fullRampDist = dist
             self.threestage = maxV1 > maxV2
             self.maxV = max(maxV1, maxV2)
-            
+
+        self.iJx6 = 6 / J
         if self.threestage:
-            self.v1 = 1/2*A*A/J
+            self.v1 = A*A/J/2
             self.v2 = self.maxV - self.v1
-            self.d1 = 1/6*A*A*A/J/J
+            self.d1 = A*A*A/J/J/6
             self.d2 = self.d1 + self.maxV*riseTime / 2
             self.iA = 1 / A
             self.AdJ = A / J
         else:
             self.tmid = math.pow(self.fullRampDist / J, 1/3)
-            self.d1 = 1/6*J*self.tmid*self.tmid*self.tmid
-            self.v1 = 1/2*J*self.tmid*self.tmid
+            self.d1 = J*self.tmid*self.tmid*self.tmid/6
+            self.v1 = J*self.tmid*self.tmid/2
             self.a1 = self.J * self.tmid
 
     def getParameters(self, distLeft):
@@ -65,8 +66,8 @@ class Profile:
         
         if self.threestage:
             if x < self.d1:
-                t = math.pow(6 * x / self.J, 1/3)
-                return (1/2*self.J*t*t,self.J*t, self.J)
+                t = math.pow(x * self.iJx6, 1/3)
+                return (self.J*t*t/2,self.J*t, self.J)
             elif x < self.d2:
                 dz = x - self.d1
                 tz = (-self.v1+math.sqrt(self.v1*self.v1+2*self.A*dz)) * self.iA
@@ -75,12 +76,12 @@ class Profile:
                 dz = x - self.d2
                 tz = binarySolveStep3(dz, self.v2, self.A, self.AdJ, self.J)
                 tq = self.AdJ - tz
-                return (self.maxV - 1/2*self.J*tq*tq, self.J*tq, -self.J)
+                return (self.maxV - self.J*tq*tq/2, self.J*tq, -self.J)
         else:
             if x < self.d1:
-                t = math.pow(6 * x / self.J, 1/3)
+                t = math.pow(x * self.iJx6, 1/3)
                 a = self.J*t
-                v = 1/2*self.J*t*t
+                v = self.J*t*t/2
                 return (v,a, self.J)
             else:
                 dz = x - self.d1
@@ -93,12 +94,18 @@ def main():
     steps = 200
     totalDist = 2
 
-    profile = Profile(totalDist, V=0.4, A=0.2, J=0.1)
+    limits = [(0.4,0.2,0.1),
+              (0.8,0.2,0.1),
+              (0.4,0.8,0.1),
+              (0.4,0.2,0.8),
+              (0.4,0.2,0.01)]   
     
-    for i in range(steps):
-        distLeft = totalDist * (1 - i / steps)
-        v,a,j = profile.getParameters(distLeft)
-        print("{: f} {: f} {: f} {: f}".format(distLeft, v,a,j))
+    for idx,p in enumerate(limits):
+        profile = Profile(totalDist, V=p[0], A=p[1], J=p[2])
+        for i in range(steps):
+            distLeft = totalDist * (i / steps)
+            v,a,j = profile.getParameters(distLeft)
+            print("{: f} {: f} {: f} {: f}".format(totalDist*idx+distLeft, v,a,j))
 
 if __name__ == "__main__":
     main()
